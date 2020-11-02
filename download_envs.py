8a866ec4288e3b0b1166e6ddc395237ea681a412
SestoAle <alessandrosestini92@gmail.com>
2020-11-02 16:28:16 +0100

    Update download_envs.py

--- download_envs.py
+++ download_envs.py
@@ -7,10 +7,10 @@ from sys import platform
 
 # Download DeepCrawl environment if it does not exist
 if platform == 'linux' or platform == 'linux2':
-    gdd.download_file_from_google_drive(file_id='1qT7JUF-72V-ov0yttJghQ87VGLBhIyDx',
-                                        dest_path='envs/envs.zip', showsize=True,
+    gdd.download_file_from_google_drive(file_id='19BlS0W7nwhrBS4E25BMdf96ICDsp7jts',
+                                        dest_path='envs/DeepCrawl-Transformer.zip', showsize=True,
                                         unzip=True)
-else:
-    gdd.download_file_from_google_drive(file_id='1-0fraEld9CSRV7vacAnddqpVdUkTWXvn',
-                                        dest_path='envs/envs.zip', showsize=True,
+
+    gdd.download_file_from_google_drive(file_id='13NLxzd7VFQPx0sC5L4nZFhlRLBGkTI2i',
+                                        dest_path='envs/DeepCrawl-Dense-Embedding.zip', showsize=True,
                                         unzip=True)
